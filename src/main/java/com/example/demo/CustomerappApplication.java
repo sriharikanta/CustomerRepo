64adf5d960680a009620cc4f3b3647ce5b5aca58
sriharikanta <60289840+sriharikanta@users.noreply.github.com>
2024-04-11 12:03:24 +0530

    Update CustomerappApplication.java

--- src/main/java/com/example/demo/CustomerappApplication.java
+++ src/main/java/com/example/demo/CustomerappApplication.java
@@ -9,6 +9,7 @@ public class CustomerappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerappApplication.class, args);
 		System.out.println("srihari");
+		System.out.println("this changes in remote");
 	}
 
 }
