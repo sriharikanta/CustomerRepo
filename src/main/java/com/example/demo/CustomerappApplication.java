08e7385f3a4e67d4aa95da0462c8551483bed6a5
sriharikanta <kanta.sriharinaidu@gmail.com>
2024-04-15 21:08:29 +0530

    Update CustomerappApplication.java
    add main sys out statement

--- src/main/java/com/example/demo/CustomerappApplication.java
+++ src/main/java/com/example/demo/CustomerappApplication.java
@@ -8,7 +8,7 @@ public class CustomerappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerappApplication.class, args);
-		System.out.println("srihari");
+       System.out.println("srihari naidu");
 	}
 
 }
